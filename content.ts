cb871af75671eeed1b761246bfb1f8fe32191ff3
Udayan Maurya <edmauryaishu@gmail.com>
2024-08-17 10:11:43 -0700

    content script

---
+++ content.ts
@@ -0,0 +1,9 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://quotes.toscrape.com/*"]
+}
+
+// window.addEventListener("load", () => {
+console.log("sss")
+// })
